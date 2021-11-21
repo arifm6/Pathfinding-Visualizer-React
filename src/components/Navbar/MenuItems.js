85429a228020acdfdde48574ac5d2a8115b5e797
Arifm6 <arifmassih6@gmail.com>
2021-11-21 00:34:05 -0500

    navigation bar starting template

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links', 
+  },
+  {
+    title: 'Visualize',
+    url: '#',
+    cName: 'nav-links', 
+  },
+  {
+    title: 'Algorithms',
+    url: '#',
+    cName: 'nav-links', 
+  },
+  {
+    title: 'Maze',
+    url: '#',
+    cName: 'nav-links', 
+  },
+  {
+    title: 'Clear',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Dropdown-mobile',
+    url: '#',
+    cName: 'nav-links-mobile'
+  },
+]
